1d14cff63edd907b0553b64d753806fa0c813dc4
Pavlo Karpenko <karpenkopasha19@outlook.com>
2023-08-26 23:55:27 +0300

    adapted header

--- js/script.js
+++ js/script.js
@@ -6,6 +6,6 @@ function documentClick(e) {
     const targetItem = e.target;
 
     if (targetItem.closest('.icon-menu')) {
-        document.documentElement.classList.toggle('menu-open');
+        document.documentElement.classList.toggle('menu__open');
     }
 }
